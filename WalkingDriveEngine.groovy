b0991ae7ce91579dcd5b1ff2f53f68128a9ae752
madhephaestus <mad.hephaestus@gmail.com>
2016-03-05 16:27:44 -0500

    fixing the walking for standard carl/n/n

--- WalkingDriveEngine.groovy
+++ WalkingDriveEngine.groovy
@@ -17,14 +17,14 @@ import com.neuronrobotics.sdk.addons.kinematics.IDriveEngine;
 
 double stepOverHeight=5;
 long stepOverTime=80;
-Double zLock=-183.8;
+Double zLock=-70;
 Closure calcHome = { DHParameterKinematics leg -> 
 		TransformNR h=leg.calcHome() 
 
 		TransformNR tr = leg.forwardOffset(new TransformNR())
 		tr.setZ(zLock)
 		
-		return tr;
+		return h;
 
 }
 boolean usePhysicsToMove = true;
